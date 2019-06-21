7c75c02e1ded4148dfc8c181d7b8bc8660e897f6
Dev Pedro <pedro.vr096@gmail.com>
2019-06-21 08:13:01 -0500

    layout with the loading component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeaderComponent from './dump/Header'
 import LandingComponent from './dump/Landing'
+import LoadingComponent from './dump/Loading';
 export default () => {
   return (
     <>
@@ -8,6 +9,7 @@ export default () => {
         title="Bienvenido a Tweet Viewer"
       />
       <LandingComponent />
+      <LoadingComponent open/>
     </>
   )
 }
