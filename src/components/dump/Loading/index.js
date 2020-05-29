5c41e2ec2a4c782bf01522cee2f53c03ea0ee791
Dev Pedro <pedrova69@gmail.com>
2020-05-28 23:32:45 -0500

    formatting

--- src/components/dump/Loading/index.js
+++ src/components/dump/Loading/index.js
@@ -1,37 +1,47 @@
-import './styles.css'
-import React, { useEffect, useState, } from 'react'
-import clsx from 'clsx'
-import TwitterLogo from 'assets/TwitterLogo'
+import "./styles.css";
+import React, { useEffect, useState } from "react";
+import clsx from "clsx";
+import TwitterLogo from "assets/TwitterLogo";
 const Loading = ({ open, text, onDidMount }) => {
-  const [afterSecond, setAfter] = useState(false)
+  const [afterSecond, setAfter] = useState(false);
   useEffect(() => {
     if (open) {
-      const timer = setTimeout(() => { setAfter(true); }, 1000);
-      return () => { clearTimeout(timer) };
+      const timer = setTimeout(() => {
+        setAfter(true);
+      }, 1000);
+      return () => {
+        clearTimeout(timer);
+      };
     }
   });
   return (
-    <div className={clsx({
-      loading: open,
-      hide: !open,
-    })}
+    <div
+      className={clsx({
+        loading: open,
+        hide: !open,
+      })}
     >
       <div className="loading__point">
-        <TwitterLogo classSvg="loading__image" classPath="loading__path" color="none" />
+        <TwitterLogo
+          classSvg="loading__image"
+          classPath="loading__path"
+          color="none"
+        />
       </div>
-      <h2 className={clsx("loading__text",
-        { "loading__text-animation": afterSecond })
-      }>
+      <h2
+        className={clsx("loading__text", {
+          "loading__text-animation": afterSecond,
+        })}
+      >
         {text}
       </h2>
-    </div >
-
-  )
-}
+    </div>
+  );
+};
 
 Loading.defaultProps = {
   open: false,
   text: "",
-  onDidMount: () => { }
-}
+  onDidMount: () => {},
+};
 export default Loading;
